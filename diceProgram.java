303997257bf4d7bd9a55118703a8c1223a0e5004
cathrine100105 <cathrine.peterraj@gmail.com>
2025-11-23 16:32:13 +0530

    updated commit

---
+++ diceProgram.java
@@ -0,0 +1,97 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class diceProgram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int noOfDice;
+        int roll;
+        int total = 0;
+
+        System.out.print("Enter the number to Dice roll: ");
+        noOfDice = scanner.nextInt();
+        if(noOfDice>0){
+            for(int i=0; i<noOfDice;i++) {
+                roll = random.nextInt(1,7);
+                printDie(roll);
+                System.out.println("You Rolled: "+ roll);
+                total = total+roll;
+            }
+            System.out.println("Total is : "+total);
+        }
+        else{
+            System.out.println("The dice roll should have Greater than 0.");
+
+        }
+    }
+
+        static void printDie(int roll){
+
+         String dice1 = """
+                 -------
+                |       |
+                |   ●   |
+                |       |
+                 -------
+                 """;
+
+
+            String dice2 = """
+                 -------
+                |●      |
+                |       |
+                |      ●|
+                 -------
+                 """;
+
+
+            String dice3 = """
+                 -------
+                |●      |
+                |   ●   |
+                |      ●|
+                 -------
+                 """;
+
+
+            String dice4 = """
+                 -------
+                |●     ●|
+                |       |
+                |●     ●|
+                 -------
+                 """;
+
+
+            String dice5 = """
+                 -------
+                |●     ●|
+                |   ●   |
+                |●     ●|
+                 -------
+                 """;
+
+
+            String dice6 = """
+                 -------
+                |●  ●  ●|
+                |       |
+                |●  ●  ●|
+                 -------
+                 """;
+
+
+         switch (roll){
+             case 1-> System.out.println(dice1);
+             case 2 ->System.out.println(dice2);
+             case 3->System.out.println(dice3);
+             case 4->System.out.println(dice4);
+             case 5->System.out.println(dice5);
+             case 6-> System.out.println(dice6);
+             default-> System.out.println("INVALID ROLL");
+         }
+
+    }
+}
